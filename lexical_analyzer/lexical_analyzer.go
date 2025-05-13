af6ee3ca3631634fcddf0a9336bf30b037872d0e
GabrielSathler <gabrielvdfs@gmail.com>
2025-05-12 22:56:57 -0300

    feat: Lexical-analyzer implemented

--- lexical_analyzer/lexical_analyzer.go
+++ lexical_analyzer/lexical_analyzer.go
@@ -1,1 +1,138 @@
 package lexical_analyzer
+
+import (
+	"bufio"
+	"io"
+	"unicode"
+
+	"github.com/GabrielSathler/Compilador-MASClang/tokens"
+)
+
+type Position struct {
+	Line   int
+	Column int
+}
+
+type Lexer struct {
+	pos    Position
+	reader *bufio.Reader
+}
+
+func NewLexer(reader io.Reader) *Lexer {
+	return &Lexer{
+		pos:    Position{Line: 1, Column: 0},
+		reader: bufio.NewReader(reader),
+	}
+}
+
+func (l *Lexer) Lex() (Position, tokens.Token, string) {
+	for {
+		r, _, err := l.reader.ReadRune()
+		if err != nil {
+			if err == io.EOF {
+				return l.pos, tokens.EOF, ""
+			}
+			panic(err)
+		}
+
+		l.pos.Column++
+
+		switch r {
+		case '\n':
+			l.resetPosition()
+		case ';':
+			return l.pos, tokens.SEMI, ";"
+		case '(':
+			return l.pos, tokens.LPAREN, "("
+		case ')':
+			return l.pos, tokens.RPAREN, ")"
+		case '{':
+			return l.pos, tokens.LBRACE, "{"
+		case '}':
+			return l.pos, tokens.RBRACE, "}"
+		case ':':
+			return l.pos, tokens.COLON, ":"
+		case ',':
+			return l.pos, tokens.COMMA, ","
+		case '+':
+			return l.pos, tokens.ADD, "+"
+		case '-':
+			return l.pos, tokens.SUB, "-"
+		case '*':
+			return l.pos, tokens.MUL, "*"
+		case '/':
+			return l.pos, tokens.DIV, "/"
+		case '=':
+			return l.pos, tokens.ASSIGN, "="
+		default:
+			if unicode.IsSpace(r) {
+				continue
+			} else if unicode.IsDigit(r) {
+				startPos := l.pos
+				l.backup()
+				lit := l.lexInt()
+				return startPos, tokens.INT, lit
+			} else if unicode.IsLetter(r) {
+				startPos := l.pos
+				l.backup()
+				lit := l.lexIdent()
+				return startPos, tokens.IDENT, lit
+			} else {
+				return l.pos, tokens.ILLEGAL, string(r)
+			}
+		}
+	}
+}
+
+func (l *Lexer) resetPosition() {
+	l.pos.Line++
+	l.pos.Column = 0
+}
+
+func (l *Lexer) backup() {
+	if err := l.reader.UnreadRune(); err != nil {
+		panic(err)
+	}
+
+	l.pos.Column--
+}
+
+func (l *Lexer) lexInt() string {
+	var lit string
+	for {
+		r, _, err := l.reader.ReadRune()
+		if err != nil {
+			if err == io.EOF {
+				return lit
+			}
+		}
+
+		l.pos.Column++
+		if unicode.IsDigit(r) {
+			lit = lit + string(r)
+		} else {
+			l.backup()
+			return lit
+		}
+	}
+}
+
+func (l *Lexer) lexIdent() string {
+	var lit string
+	for {
+		r, _, err := l.reader.ReadRune()
+		if err != nil {
+			if err == io.EOF {
+				return lit
+			}
+		}
+
+		l.pos.Column++
+		if unicode.IsLetter(r) {
+			lit = lit + string(r)
+		} else {
+			l.backup()
+			return lit
+		}
+	}
+}
--- tokens/tokens.go
+++ tokens/tokens.go
@@ -3,58 +3,77 @@ package tokens
 type Token int
 
 const (
-	EOF     = "EOF"
-	ILLEGAL = "ILLEGAL"
+	EOF Token = iota
+	ILLEGAL
 
-	IDENT  = "IDENT"
-	INT    = "INT"
-	FLOAT  = "FLOAT"
-	CHAR   = "CHAR"
-	BOOL   = "BOOL"
-	STRING = "STRING"
+	IDENT
+	INT
+	FLOAT
+	CHAR
+	BOOL
+	STRING
 
-	ASSIGN = "="
-	ADD    = "+"
-	SUB    = "-"
-	MUL    = "*"
-	DIV    = "/"
+	ASSIGN
+	ADD
+	SUB
+	MUL
+	DIV
 
-	SEMI   = ";"
-	LPAREN = "("
-	RPAREN = ")"
-	LBRACE = "{"
-	RBRACE = "}"
-	COLON  = ":"
-	COMMA  = ","
+	SEMI
+	LPAREN
+	RPAREN
+	LBRACE
+	RBRACE
+	COLON
+	COMMA
 
-	VAR    = "var"
-	FUNC   = "func"
-	IF     = "if"
-	ELSE   = "else"
-	WHILE  = "while"
-	FOR    = "for"
-	RETURN = "return"
-	PRINT  = "print"
-	INPUT  = "input"
+	VAR
+	FUNC
+	IF
+	ELSE
+	WHILE
+	FOR
+	RETURN
+	PRINT
+	INPUT
 )
 
-var Tokens = map[string]string{
-	"var":    VAR,
-	"func":   FUNC,
-	"if":     IF,
-	"else":   ELSE,
-	"while":  WHILE,
-	"for":    FOR,
-	"return": RETURN,
-	"print":  PRINT,
-	"input":  INPUT,
+var tokens = []string{
+	EOF:     "EOF",
+	ILLEGAL: "ILLEGAL",
 
-	"int":    INT,
-	"float":  FLOAT,
-	"char":   CHAR,
-	"bool":   BOOL,
-	"string": STRING,
+	IDENT:  "IDENT",
+	INT:    "INT",
+	FLOAT:  "FLOAT",
+	CHAR:   "CHAR",
+	BOOL:   "BOOL",
+	STRING: "STRING",
 
-	"true":  BOOL,
-	"false": BOOL,
+	ASSIGN: "=",
+	ADD:    "+",
+	SUB:    "-",
+	MUL:    "*",
+	DIV:    "/",
+
+	SEMI:   ";",
+	LPAREN: "(",
+	RPAREN: ")",
+	LBRACE: "{",
+	RBRACE: "}",
+	COLON:  ":",
+	COMMA:  ",",
+
+	VAR:    "var",
+	FUNC:   "func",
+	IF:     "if",
+	ELSE:   "else",
+	WHILE:  "while",
+	FOR:    "for",
+	RETURN: "return",
+	PRINT:  "print",
+	INPUT:  "input",
+}
+
+func (t Token) String() string {
+	return tokens[t]
 }
